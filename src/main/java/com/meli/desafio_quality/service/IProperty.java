cb3af1d14e2c53597677b28c7cc65dc35b9a0599
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-14 12:33:52 -0300

    feat: implement PropertyService
    Remove GET methods and implement POST method

--- src/main/java/com/meli/desafio_quality/service/IProperty.java
+++ src/main/java/com/meli/desafio_quality/service/IProperty.java
@@ -1,16 +1,10 @@
 package com.meli.desafio_quality.service;
 
-import com.meli.desafio_quality.exception.NotFoundException;
+import com.meli.desafio_quality.dto.PropertyDTO;
 import com.meli.desafio_quality.model.Property;
-
-import java.util.List;
 
 public interface IProperty {
 
-    Property getProperty(String propName) throws NotFoundException;
-
-    List<Property> getAllProperties();
-
-    void createProperty(Property property);
+    PropertyDTO processProperty(Property property);
 
 }

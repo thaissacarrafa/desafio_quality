adac0ef280cc442228bcf6db0a54989e3bb45f42
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-14 12:34:06 -0300

    feat: implement PropertyController
    Remove GET methods and implement POST method

--- src/main/java/com/meli/desafio_quality/controller/PropertyController.java
+++ src/main/java/com/meli/desafio_quality/controller/PropertyController.java
@@ -1,35 +1,22 @@
 package com.meli.desafio_quality.controller;
 
+import com.meli.desafio_quality.dto.PropertyDTO;
 import com.meli.desafio_quality.model.Property;
 import com.meli.desafio_quality.service.IProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/properties")
+@RequestMapping("/property")
 public class PropertyController {
 
     @Autowired
     private IProperty service;
 
-    @GetMapping("/{propName}")
-    public ResponseEntity<Property> getProperty(@PathVariable String propName) {
-        return ResponseEntity.ok(service.getProperty(propName));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Property>> getAllProperties() {
-        return ResponseEntity.ok(service.getAllProperties());
-    }
-
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProperty(@RequestBody Property property) {
-        service.createProperty(property);
+    public ResponseEntity<PropertyDTO> processProperty(@RequestBody Property property) {
+        return ResponseEntity.ok(service.processProperty(property));
     }
 
 }

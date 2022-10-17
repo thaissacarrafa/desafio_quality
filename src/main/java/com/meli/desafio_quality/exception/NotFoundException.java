34b84675ce796716ef8ea47ef8372c4795fc4286
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 08:05:43 -0300

    fix: delete unused files

--- src/main/java/com/meli/desafio_quality/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.meli.desafio_quality.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}

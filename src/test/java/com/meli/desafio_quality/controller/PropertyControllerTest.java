011fad483fcfceb87f29efabcd1be2c48aa9f820
igor <igor.fernandes@mercadolivre.com>
2022-10-17 11:20:05 -0300

    feat: implements test framework

---
+++ src/test/java/com/meli/desafio_quality/controller/PropertyControllerTest.java
@@ -0,0 +1,12 @@
+package com.meli.desafio_quality.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyControllerTest {
+
+    @Test
+    void processProperty() {
+    }
+}
---
+++ src/test/java/com/meli/desafio_quality/service/PropertyServiceTest.java
@@ -0,0 +1,28 @@
+package com.meli.desafio_quality.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyServiceTest {
+
+    @Test
+    void getLargestRoom() {
+    }
+
+    @Test
+    void getRoomsFormatted() {
+    }
+
+    @Test
+    void getPropArea() {
+    }
+
+    @Test
+    void getPropValue() {
+    }
+
+    @Test
+    void processProperty() {
+    }
+}

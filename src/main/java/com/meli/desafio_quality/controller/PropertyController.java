e9cf18e0b0f7335302576edbb7320c25483a2cf7
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 08:07:31 -0300

    fix: update endpoint mapping

--- src/main/java/com/meli/desafio_quality/controller/PropertyController.java
+++ src/main/java/com/meli/desafio_quality/controller/PropertyController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/property")
+@RequestMapping("/properties")
 public class PropertyController {
 
     @Autowired
